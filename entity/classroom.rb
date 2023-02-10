baf200ceb6241290a7d0f2256a032a8464d8d602
hopeforkoami <hopeforkoami@gmail.com>
2023-02-10 19:39:41 +0000

    Add relationship between Classroom and Student

--- entity/classroom.rb
+++ entity/classroom.rb
@@ -1,6 +1,13 @@
 class Classroom
+  attr_reader :Students
   attr_accessor :label
   def initialize(class_label)
     @label = class_label
+    @Students =[]
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
   end
 end
