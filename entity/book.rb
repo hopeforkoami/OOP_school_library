d3893ef62ed9be1c5871a49e31c8c4db6514539d
hopeforkoami <hopeforkoami@gmail.com>
2023-02-10 19:55:49 +0000

    Add relationship between Person and Book
    Add many to many relationship between Person and Book using the intermediate class Rental

--- entity/book.rb
+++ entity/book.rb
@@ -1,7 +1,8 @@
 class Book
-  attr_accessor :title :author
+  attr_accessor :title, :author, :rentals
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
 end
--- entity/person.rb
+++ entity/person.rb
@@ -1,7 +1,7 @@
 require './interface/nameable'
 class Person < Nameable
-  attr_reader :id
-  attr_accessor :name, :age
+  attr_reader :id 
+  attr_accessor :name, :age ,:rentals
 
   def initialize(age, name, parent_permission: true)
     super
@@ -9,6 +9,7 @@ class Person < Nameable
     @name = name
     @age = age
     @permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
